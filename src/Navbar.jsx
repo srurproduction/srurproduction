ef3e2b72129ba3543919cd40b82d70003ac990c0
Aman raj <amanraj300607@gmail.com>
2025-06-11 14:09:24 +0530

    make navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './assets/logo.jpeg'; 
 
 export default function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top shadow-sm">
+    <nav className="navbar navbar-expand-lg navbar-light sticky-top shadow-sm" style={{
+    background: "linear-gradient(135deg, #8B5E3C, #D8A055)", boxShadow: "0 10px 25px rgba(0, 0, 0, 0.15)"
+}}>
       <div className="container">
-        <a className="navbar-brand fw-bold" href="#">RiyaDesigns</a>
+        <img src={logo} alt="" style={{height:'50px',width:'50px',borderRadius:'10px'}}/>
+        <a className="navbar-brand fs-3 fw-bolder px-4 " style={{color:'white'}} href="#">Srur Production </a>
         <div className="ms-auto">
-          <a href="#services" className="me-3 text-decoration-none">Services</a>
-          <a href="#gallery" className="me-3 text-decoration-none">Gallery</a>
-          <a href="#testimonials" className="me-3 text-decoration-none">Testimonials</a>
-          <a href="#contact" className="text-decoration-none">Contact</a>
+          <a href="#services" className="me-3 fs-5 fw-semibold text-white text-decoration-none">Services</a>
+          <a href="#gallery" className="me-3 fs-5 fw-semibold text-white text-decoration-none">Gallery</a>
+          <a href="#testimonials" className="me-3 fs-5 fw-semibold text-white text-decoration-none">Testimonials</a>
+          <a href="#contact" className=" text-white fs-5 fw-semibold text-decoration-none">Contact</a>
         </div>
       </div>
     </nav>
